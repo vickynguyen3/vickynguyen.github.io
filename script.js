6bef5e9f5f42a637a54bd5396fd7552e1786e0de
Vicky Nguyen <78074332+vickynguyen3@users.noreply.github.com>
2024-08-26 22:13:23 -0500

    ux project 1 png

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Smooth scrolling for navigation links
+document.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.querySelector('nav a[href="#home"]');
+    const portfolioLink = document.querySelector('nav a[href="#portfolio"]');
+    
+    // Portfolio link click event
+    portfolioLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector('#portfolio').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    // Home link click event
+    homeLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        document.querySelector('#home').scrollIntoView({ behavior: 'smooth' });
+    });
+
+});
